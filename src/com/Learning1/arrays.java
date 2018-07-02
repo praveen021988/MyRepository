ad463cb2f15d379a6c500f1af61fcdcbe1c4e679
Praveen <praveen.maande@gmail.com>
2018-07-02 09:09:32 +0530

    arrays.java

---
+++ src/com/Learning1/arrays.java
@@ -0,0 +1,19 @@
+package com.Learning1;
+
+public class arrays {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		int x[]={10,20,30,40};
+		System.out.println(x.length);
+		
+		for (int i=0; i<x.length;i++)
+		{
+			System.out.println(x[i]);
+		}
+
+	}
+
+}
